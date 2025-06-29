569cbfe90b8e3e25628d29cb4134d4d63e6902c9
MahderAkalu <mahderakaludemissie@gmail.com>
2025-06-28 21:02:10 -0400

    updated and deploy functionality added

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,69 +3,44 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [
-        {
-            name: "Snake Plant",
-            image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-            cost: "$15",
-        },
-        {
-            name: "Spider Plant",
-            image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-            cost: "$12",
-        },
-        {
-            name: "Peace Lily",
-            image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
-            cost: "$18",
-        },
-        {
-            name: "Boston Fern",
-            image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
-            cost: "$20",
-        },
-    
-        {
-            name: "Rubber Plant",
-            image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
-            cost: "$17",
-        },
-        {
-            name: "Aloe Vera",
-            image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-            cost: "$14",
-        },
-        {
-            name: "Rosemary",
-            image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg",
-            cost: "$15",
-        },
-
-
-    ], // Initialize items as an empty array
+    items: [], // Initialize items as an empty array
+    numOfItems: 0
   },
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find(item => item.name === name);
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.name === newItem.name);
 
       if (existingItem) {
         // If the item already exists, increase its quantity
         existingItem.quantity += 1;
       } else {
         // If it's a new item, add it to the cart with quantity 1
-        state.items.push({ name, image, cost, quantity: 1 });
+        state.items.push({ ...newItem, quantity: 1 });
       }
+      state.numOfItems += 1;
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        const itemName = action.payload;
+        const existingItem = state.items.find(item => item.name === itemName);
+
+        if (existingItem) {
+            // Update the total number of items
+            state.numOfItems -= existingItem.quantity;
+            state.items = state.items.filter(item => item.name !== itemName);
+        }
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-      }
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        const differenceQuantity = quantity - existingItem.quantity;
+        existingItem.quantity = quantity;
+
+        // Update the total number of items in the cart
+        state.numOfItems += differenceQuantity;
+    }
     },
   },
 });
